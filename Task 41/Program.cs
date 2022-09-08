23e06cc79dca07deb15d3d4ba7d25db4b1647c88
Natalia <natashaka27@mail.ru>
2022-09-09 00:19:45 +0500

    .\Task 41

---
+++ Task 41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int size = NumberEnteredByUser("Введите количество цифр: ", "Ошибка ввода!");
+int[] arrayNum = GetArray(size);
+int countNumGreaterZero = GetCountOfNumbersGreaterThanZero(arrayNum);
+
+Console.WriteLine($"В перечне {String.Join(", ", arrayNum)} количество чисел больше 0 = {countNumGreaterZero}");
+
+int NumberEnteredByUser(string message, string messageError)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool correctParse = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (correctParse)
+            return userNumber;
+        Console.WriteLine(messageError);
+    }
+}
+
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int userNumber;
+        while (true)
+        {
+            Console.WriteLine("Введите {0}-й элемент", i + 1);
+            bool isCorrect = int.TryParse(Console.ReadLine(), out userNumber);
+            if (isCorrect)
+                break;
+            Console.WriteLine("Ошибка, введите число");
+            }
+        arr[i] = userNumber;
+    }
+    return arr;
+}
+
+int GetCountOfNumbersGreaterThanZero(int[] arr)
+{
+    int count = 0;
+    foreach (int element in arr)
+    {
+       if (element > 0)
+       count++;
+    }
+    return count;
+}
